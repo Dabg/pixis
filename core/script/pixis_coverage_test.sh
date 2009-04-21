3852673a03d9b12c8a3b73e5ff094c0a3e4aba9b
danjou <danjou@u-210160014157.hotspot.ne.jp>
2009-04-21 11:48:38 +0900

    mechanized test sample

--- core/script/pixis_coverage_test.sh
+++ core/script/pixis_coverage_test.sh
@@ -23,7 +23,7 @@ use_ok \$_ for @modules;
 rm -rf cover_db
 perl Makefile.PL
 MEMCACHED_SERVER=127.0.0.1:11211 \
-    HARNESS_PERL_SWITCHES=-MDevel::Cover=+ignore,inc,-coverage,statement,branch,condition,path,subroutine \
+    HARNESS_PERL_SWITCHES=-MDevel::Cover=+ignore,inc,tt2,-coverage,statement,branch,condition,path,subroutine \
     make test
 cover
 rm -f t/00_allload.t
