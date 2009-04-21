2b88a142b3674d8a1f85d23412f0d8381038fee5
danjou <danjou@u-210160014157.hotspot.ne.jp>
2009-04-21 10:26:57 +0900

    cover plugins

--- core/script/pixis_coverage_test.sh
+++ core/script/pixis_coverage_test.sh
@@ -9,6 +9,7 @@ my @modules = qw(
 " > t/00_allload.t
 
 find lib -name "*.pm" | sed "s/lib\//   /;s/\.pm//;s/\//::/g" >> t/00_allload.t
+find ../plugins/*/lib -name "*.pm" | sed "s/^.*lib\//   /;s/\.pm//;s/\//::/g" >> t/00_allload.t
 
 echo "
 );
@@ -21,7 +22,9 @@ use_ok \$_ for @modules;
 
 rm -rf cover_db
 perl Makefile.PL
-HARNESS_PERL_SWITCHES=-MDevel::Cover=+ignore,inc,-coverage,statement,branch,condition,path,subroutine make test
+MEMCACHED_SERVER=127.0.0.1:11211 \
+    HARNESS_PERL_SWITCHES=-MDevel::Cover=+ignore,inc,-coverage,statement,branch,condition,path,subroutine \
+    make test
 cover
 rm -f t/00_allload.t
 rm -rf cover_db_view
