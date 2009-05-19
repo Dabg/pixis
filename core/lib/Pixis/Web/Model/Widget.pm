0de408acda30837afe5f079bdf0d21abd8b35b8c
lestrrat <lestrrat+github@gmail.com>
2009-05-19 15:52:34 +0900

    cleanup

--- core/lib/Pixis/Web/Model/Widget.pm
+++ core/lib/Pixis/Web/Model/Widget.pm
@@ -1,12 +1,19 @@
 package Pixis::Web::Model::Widget;
 use Moose;
+use MooseX::AttributeHelpers;
+use namespace::clean -except => qw(meta);
 
 BEGIN { extends 'Catalyst::Model' }
 
 has widgets => (
+    metaclass => 'Collection::Has',
     is => 'ro',
     isa => 'HashRef',
     default => sub { +{} },
+    provides => {
+        set => 'widget_set',
+        get => 'widget_get',
+    }
 );
 
 sub load {
@@ -18,16 +25,17 @@ sub load {
         confess "No type passed to load";
     }
 
-    my $widget = $self->widgets->{ $type };
+    my $widget = $self->widget_get( $type );
 
     if (! $widget) {
         my $class = "Pixis::Widget::$type";
         Class::MOP::load_class($class);
         $widget = $class->new();
-        $self->widgets->{$type} = $widget;
+        $self->widget_set( $type, $widget );
     }
     return $widget;
 }
 
 __PACKAGE__->meta->make_immutable;
 
+1;
