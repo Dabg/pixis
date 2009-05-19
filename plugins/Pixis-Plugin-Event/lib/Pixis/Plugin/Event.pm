3ab0266fc71ac1940f3ce7121520d7bb3b407291
lestrrat <lestrrat+github@gmail.com>
2009-05-19 21:33:39 +0900

    fixe plugin init

--- plugins/Pixis-Plugin-Event/lib/Pixis/Plugin/Event.pm
+++ plugins/Pixis-Plugin-Event/lib/Pixis/Plugin/Event.pm
@@ -10,4 +10,14 @@ has '+extra_api' => (
     }
 );
 
+after register => sub {
+    my $registry = Pixis::Registry->instance;
+    my $c = $registry->get(pixis => 'web');
+
+    my $left = $c->model('Widget')->load('LeftNavigation');
+    $left->item_add(
+        { id => 'events', uri => '/event', text => $c->localize('Event') }
+    );
+};
+
 __PACKAGE__->meta->make_immutable;
