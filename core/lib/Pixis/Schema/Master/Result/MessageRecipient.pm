8fe38cdebf444ba98aaab0ed73493159f7b5b053
船木大郎 <taro@maki-daisuke-no-macbook-2.local>
2009-06-03 17:20:00 +0900

    remove Profile.pm

--- core/lib/Pixis/Schema/Master/Result/MessageRecipient.pm
+++ core/lib/Pixis/Schema/Master/Result/MessageRecipient.pm
@@ -31,6 +31,8 @@ __PACKAGE__->add_columns(
 );
 __PACKAGE__->set_primary_key('id');
 __PACKAGE__->belongs_to( 'message' => 'Pixis::Schema::Master::Result::Message', 'message_id' );
+=pod
 __PACKAGE__->belongs_to( 'to_profile' => 'Pixis::Schema::Master::Result::Profile', 'to_profile_id' );
+=cut
 
 1;
