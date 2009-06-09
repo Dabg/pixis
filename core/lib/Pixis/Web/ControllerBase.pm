ec33e6e62d0c7380ca2dd85f6b11809cbf3245da
lestrrat <lestrrat+github@gmail.com>
2009-06-09 22:00:32 +0900

    documentation

--- core/lib/Pixis/Web/ControllerBase.pm
+++ core/lib/Pixis/Web/ControllerBase.pm
@@ -83,3 +83,79 @@ sub form {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+Pixis::Web::ControllerBase - Base Controller Class
+
+=head1 SYNOPSIS
+
+    package MyApp::Controller::Foo;
+    use Moose;
+    use namespace::clean -except => qw(meta);
+    BEGIN { extends 'Pixis::Web::ControllerBase' }
+
+=head1 CONFIGURATION
+
+    # every controller derived from Pixis::Web::ControllerBase can be
+    # configured for authentication
+    Controller::Open:
+        default_auth: 0   # no authentication
+
+    Controller::Closed:
+        default_auth: 1   # do authentication
+
+    Controller::AdminOnly
+        default_auth:
+            - admin       # Catalyst::Plugin::Authorization::Role role
+
+    Controller::WithExceptions
+        default_auth: 1   # require auth by default
+        auth_info:
+            no_auth: 0    # bu no_auth action does not require auth
+
+=head2 ACTIONS
+
+=head2 auto
+
+Does automatic check for authentication. If authentication is required, then
+checks if the user is logged in via /auth/assert_logged_in
+
+=head1 METHODS
+
+=head2 requires_auth($action)
+
+Checks if the specified action require authentication
+
+=head2 form($c [, [ $filename | \%args ]])
+
+Returns a HTML::FormFu object. The object is already initialized, ready to be
+checked with submitted_and_valid().
+
+When called with just the context object, looks for a form config file based
+on the current action name.
+
+    sub index :Index {
+        my ($self, $c) = @_;
+
+        $self->form($c); # attempts to load index.yml (or whatever file format)
+    }
+
+When called with a context object and a scalar, attempts to load the named
+config file
+
+    sub index :Index {
+        my ($self, $c) = @_;
+
+        $self->form($c, 'foo/bar/baz');
+            # attempts to load foo/bar/baz.yml (or whatever file format)
+    }
+
+and finally, if you pass a hashref, you can expect it to be passed to 
+HTML::FormFu's constructor. If you include a filename argument, this is
+taken as the config file name. Otherwise, the action name is used, just
+like the default single argument case.
+
+=cut
