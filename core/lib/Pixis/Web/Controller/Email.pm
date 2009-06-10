976c75e78b2af7cb0ac29d732ed13010a321be0b
lestrrat <lestrrat+github@gmail.com>
2009-06-10 10:16:43 +0900

    Make Controller::Email more configurable + docs

--- core/lib/Pixis/Web/Controller/Email.pm
+++ core/lib/Pixis/Web/Controller/Email.pm
@@ -1,37 +1,62 @@
 package Pixis::Web::Controller::Email;
 use Moose;
 use namespace::clean -except => qw(meta);
-
-BEGIN { extends 'Catalyst::Controller' }
-
 use utf8;
 use Encode ();
 
-sub send :Private {
+BEGIN { extends 'Pixis::Web::ControllerBase' }
+
+has headers => (
+    is => 'ro',
+    isa => 'HashRef',
+    lazy_build => 1,
+);
+
+has mime_encoding_map => (
+    is => 'ro',
+    isa => 'HashRef',
+    lazy_build => 1
+);
+
+sub _build_headers {
+    return {
+        Charset          => 'iso-2022-jp',
+        Content_Type     => 'text/plain',
+        Content_Encoding => '7bit',
+    }
+}
+
+sub _build_mime_encoding_map {
+    return {
+        'iso-2022-jp' => 'MIME-Header-ISO_2022_JP',
+    }
+}
+
+sub send
+    :Private
+{
     my ($self, $c, $args) = @_;
 
-    my $config = $self->config;
-    my $header = $args->{header} || {};
+    my $header = 
+        Catalyst::Utils::merge_hashes($self->headers, $args->{header} || {});
+
     if (! $header->{To} ) {
         Carp::confess("Missing 'To' header");
     }
 
-    # XXX Some header are Japanese specific... do we generalize it?
-    $header->{From}             ||= $config->{From};
-    $header->{Subject}          ||= $config->{Subject};
-    $header->{Content_Encoding} ||= '7bit';
-
-    $header->{Cc}  ||= $config->{Cc} if $config->{Cc};
-    $header->{Bcc} ||= $config->{Bcc} if $config->{Bcc};
-
     if ($header->{Subject}) {
-        $header->{Subject} = Encode::encode('MIME-Header-ISO_2022_JP', $header->{Subject});
+        my $encoding = $self->mime_encoding_map->{ $header->{Charset} };
+        $header->{Subject} = Encode::encode($encoding, $header->{Subject});
     }
-    my $body = Encode::encode('iso-2022-jp', $args->{body});
+    my $body = Encode::encode($header->{Charset}, $args->{body});
+
+    if ($header->{Content_Type} !~ /charset=/) {
+        $header->{Content_Type} = "$header->{Content_Type}; charset=$header->{Charset}";
+    }
 
     my %args = (
         header       => [%$header],
-        content_type => 'text/plain; charset=iso-2022-jp',
+        content_type => $header->{Content_Type},
         body         => $body,
     );
     local $c->stash->{email} = \%args;
@@ -41,3 +66,34 @@ sub send :Private {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+Pixis::Web::Controller::Email - Send Emails
+
+=head1 SYNOPSIS
+
+    $c->forward('/email/send',
+        [ {
+            headers => {
+                To => 'somebody@mydomain',
+                Subject => "Hello!",
+            },
+            body => "Blah Blah Blah"
+        } ]
+    );
+
+=head1 ATTRIBUTES
+
+=head2 headers
+
+Holds the default set of headers to use. You can always override the headers
+by passing them in the argument to send().
+
+=head2 mime_encoding_map
+
+Holds a map from charset to MIME-Heaer-XXXX encoding name.
+
+=cut
