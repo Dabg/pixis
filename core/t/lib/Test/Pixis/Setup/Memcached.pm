56247aa0ef31ea29b16943a7560d919a00cdc834
lestrrat <lestrrat+github@gmail.com>
2009-06-17 09:21:22 +0900

    typo

--- core/t/lib/Test/Pixis/Setup/Memcached.pm
+++ core/t/lib/Test/Pixis/Setup/Memcached.pm
@@ -11,7 +11,7 @@ has memcached_servers => (
     metaclass => 'Collection::Array',
     is => 'rw',
     isa => 'ArrayRef[Str]',
-    default => sub { +[ '127.0.0.:11211' ] },
+    default => sub { +[ '127.0.0.1:11211' ] },
     provides => {
         elements => 'all_memcached_servers'
     },
