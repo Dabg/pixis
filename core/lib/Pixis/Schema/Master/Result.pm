99bfe1f8cc513a97384ac5bc15e3d1cc8485ea84
lestrrat <lestrrat+github@gmail.com>
2009-07-29 03:51:13 +0900

    allow uuid generation

--- core/lib/Pixis/Schema/Master/Result.pm
+++ core/lib/Pixis/Schema/Master/Result.pm
@@ -4,14 +4,31 @@ use MooseX::NonMoose;
 
 extends 'DBIx::Class';
 
+has uuid_gen => (
+    is => 'ro',
+    isa => 'Data::UUID',
+    lazy_build => 1,
+);
+
 __PACKAGE__->mk_classdata('engine' => 'InnoDB');
 __PACKAGE__->mk_classdata('charset' => 'UTF8');
+
+sub _build_uuid_gen {
+    require Data::UUID;
+    return Data::UUID->new()
+}
+
+sub uuid {
+    my $self = shift;
+    return $self->uuid_gen->create_str()
+}
 
 sub sqlt_deploy_hook {
     my ($self, $sqlt_table) = @_;
 
     $sqlt_table->extra->{mysql_table_type} = $self->engine;
     $sqlt_table->extra->{mysql_charset}    = $self->charset;
+
     return ();
 };
 
