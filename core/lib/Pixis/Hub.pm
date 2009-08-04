263d645d3092780fcbe4b9213c20dfac5c9df6fe
lestrrat <lestrrat+github@gmail.com>
2009-08-05 08:53:39 +0900

    silence warning

--- core/lib/Pixis/Hub.pm
+++ core/lib/Pixis/Hub.pm
@@ -3,7 +3,11 @@ use Moose::Role;
 use Pixis::Registry;
 use namespace::clean -except => qw(meta);
 
-sub api    { Pixis::Registry->get(api => $_[1] or confess "No API name provided") }
+sub api    {
+    $_[1] or confess "No API name provided";
+     Pixis::Registry->get(api => $_[1]);
+}
+
 sub schema { Pixis::Registry->get(schema => $_[1] || 'master') }
 
 1;
