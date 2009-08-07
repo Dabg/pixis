b1ff4e1cc3dd120d89b8069233953bd1273ee35a
lestrrat <lestrrat+github@gmail.com>
2009-08-07 16:51:43 +0900

    fix stupid merge

--- core/lib/Pixis/Schema/Master/Result.pm
+++ core/lib/Pixis/Schema/Master/Result.pm
@@ -4,8 +4,24 @@ use MooseX::NonMoose;
 
 extends 'DBIx::Class';
 
+has uuid_gen => (
+    is => 'ro',
+    isa => 'Data::UUID',
+    lazy_build => 1,
+);
+
 __PACKAGE__->mk_classdata('engine' => 'InnoDB');
 __PACKAGE__->mk_classdata('charset' => 'UTF8');
+
+sub _build_uuid_gen {
+    require Data::UUID;
+    return Data::UUID->new()
+}
+
+sub uuid {
+    my $self = shift;
+    return $self->uuid_gen->create_str()
+}
 
 sub sqlt_deploy_hook {
     my ($self, $sqlt_table) = @_;
